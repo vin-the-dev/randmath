e6d5b0c8cebe212846b1c16848e0c053dcac312a
Vineeth Vijayan <vinbhai4u@gmail.com>
2016-07-21 00:52:15 +0530

    Testing local notification

--- randmath/AppDelegate.swift
+++ randmath/AppDelegate.swift
@@ -16,6 +16,28 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let textAction = UIMutableUserNotificationAction()
+        textAction.identifier = "TEXT_ACTION"
+        textAction.title = "Reply"
+        textAction.activationMode = .Background
+        textAction.authenticationRequired = false
+        textAction.destructive = false
+        textAction.behavior = .TextInput
+        
+        let category = UIMutableUserNotificationCategory()
+        category.identifier = "CATEGORY_ID"
+        category.setActions([textAction], forContext: .Default)
+        category.setActions([textAction], forContext: .Minimal)
+        
+        let categories = NSSet(object: category) as! Set<UIUserNotificationCategory>
+        let settings = UIUserNotificationSettings(forTypes: [.Alert, .Badge, .Sound], categories: categories)
+        UIApplication.sharedApplication().registerUserNotificationSettings(settings)
+        
+        
+//        let notificationSettings = UIUserNotificationSettings(forTypes: [.Alert, .Badge, .Sound], categories: nil)
+//        UIApplication.sharedApplication().registerUserNotificationSettings(notificationSettings)
+        
         return true
     }
 
@@ -40,7 +62,41 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
+    
+    func application(application: UIApplication, handleActionWithIdentifier identifier: String?, forLocalNotification notification: UILocalNotification, withResponseInfo responseInfo: [NSObject : AnyObject], completionHandler: () -> Void) {
+        
+        let reply = responseInfo[UIUserNotificationActionResponseTypedTextKey] as! String
 
+        NSUserDefaults.standardUserDefaults().setObject(reply, forKey: "answerKey")
+        
+        var message = ""
+        
+        print(identifier)
+        print(responseInfo)
+        print(reply)
+        
+        if let savedValue = NSUserDefaults.standardUserDefaults().stringForKey("expectedAnswerKey") {
+            if savedValue == reply{
+                message = "Correct"
+            }
+            else{
+                message = "Wrong"
+            }
+        }
+        
+        let now: NSDateComponents = NSCalendar.currentCalendar().components([.Hour, .Minute], fromDate: NSDate())
+        
+        let cal = NSCalendar(calendarIdentifier: NSCalendarIdentifierGregorian)!
+        let date = cal.dateBySettingHour(now.hour, minute: now.minute + 1, second: now.second, ofDate: NSDate(), options: NSCalendarOptions())
+        let reminder = UILocalNotification()
+        reminder.fireDate = date
+        reminder.alertBody = message
+        reminder.alertAction = "Cool"
+        reminder.soundName = "sound.aif"
+        reminder.category = "REPLY_ID"
+        
+        UIApplication.sharedApplication().scheduleLocalNotification(reminder)
+    }
 
 }
 
--- randmath/ViewController.swift
+++ randmath/ViewController.swift
@@ -32,11 +32,15 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         _lblAnswer.text = " "
         getNewQuestion()
+        
+        if let savedValue = NSUserDefaults.standardUserDefaults().stringForKey("answerKey") {
+            print(savedValue)
+        }
     }
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
-        
+        scheduleLocal()
     }
     
     override func viewDidAppear(animated: Bool) {
@@ -135,6 +139,34 @@ class ViewController: UIViewController {
             _lblEquation.text = ""
             break
         }
+    }
+    
+    func scheduleLocal() {
+//        let notification = UILocalNotification()
+//        notification.fireDate = NSDate(timeIntervalSinceNow: 50)
+//        notification.alertBody = "Hey you! Yeah you! Swipe to unlock!"
+//        notification.alertAction = "be awesome!"
+//        notification.soundName = UILocalNotificationDefaultSoundName
+//        notification.userInfo = ["CustomField1": "w00t"]
+//        UIApplication.sharedApplication().scheduleLocalNotification(notification)
+        
+        let now: NSDateComponents = NSCalendar.currentCalendar().components([.Hour, .Minute], fromDate: NSDate())
+        
+        let cal = NSCalendar(calendarIdentifier: NSCalendarIdentifierGregorian)!
+        let date = cal.dateBySettingHour(now.hour, minute: now.minute + 1, second: 0, ofDate: NSDate(), options: NSCalendarOptions())
+        let reminder = UILocalNotification()
+        reminder.fireDate = date
+        reminder.alertBody = _lblFirstNumber.text! + _lblEquation.text! + _lblSecondNumber.text!
+        reminder.alertAction = "Cool"
+        reminder.soundName = "sound.aif"
+        reminder.category = "CATEGORY_ID"
+        
+        UIApplication.sharedApplication().scheduleLocalNotification(reminder)
+        
+        print("Firing at \(now.hour):\(now.minute+1)")
+        
+        NSUserDefaults.standardUserDefaults().setObject(expectedAnswer, forKey: "expectedAnswerKey")
+        
     }
 
 }
